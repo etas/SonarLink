1862d929d0224f46227dd6d3becf4a15844e468c
Brian Gatt <brian@ascentsoftware.eu>
2019-06-10 16:44:34 +0200

    Add AppVeyor CI support

---
+++ SetVersion.ps1
@@ -0,0 +1,12 @@
+Param([parameter(Mandatory=$true)] [string] $version)
+
+$files_to_update = @(
+  "SonarLink.API\Properties\AssemblyInfo.cs",
+  "SonarLink.TE\Properties\AssemblyInfo.cs",
+  "SonarLink.TE\source.extension.vsixmanifest",
+  "SonarLink.TE.2019\source.extension.vsixmanifest"
+)
+
+$files_to_update | ForEach-Object {
+  (Get-Content $_) | ForEach-Object { $_.replace("0.0.0.0", $version) } | Set-Content $_
+}

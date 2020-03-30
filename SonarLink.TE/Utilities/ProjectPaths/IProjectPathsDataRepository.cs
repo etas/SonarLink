21c8424c02201f731132ccc7e5ae45d6dd4502d4
Marlon Vella <marlon@ascentsoftware.eu>
2020-03-30 12:02:35 +0200

    Save project paths

---
+++ SonarLink.TE/Utilities/ProjectPaths/IProjectPathsDataRepository.cs
@@ -0,0 +1,20 @@
+﻿// (C) Copyright 2020 ETAS GmbH (http://www.etas.com/)
+
+namespace SonarLink.TE.Utilities
+{
+    /// <summary>
+    /// Interface of a repository for persisting the project paths data
+    /// </summary>
+    public interface IProjectPathsDataRepository
+    {
+        /// <summary>
+        /// Sonar projects and local path associations
+        /// </summary>
+        ProjectPathData Data { get; }
+
+        /// <summary>
+        /// Persist project paths data
+        /// </summary>
+        void Save();
+    }
+}
---
+++ SonarLink.TE/Utilities/ProjectPaths/IProjectPathsManager.cs
@@ -0,0 +1,24 @@
+﻿// (C) Copyright 2020 ETAS GmbH (http://www.etas.com/)
+
+namespace SonarLink.TE.Utilities
+{
+    /// <summary>
+    /// Manages access to and persistence of the Sonar projects and local path associatons
+    /// </summary>
+    public interface IProjectPathsManager
+    {
+        /// <summary>
+        /// Add Sonar project and local path
+        /// </summary>
+        /// <param name="project">Unique key associated with the Sonar project</param>
+        /// <param name="path">Local path associated with the Sonar project</param>
+        void Add(string project, string path);
+
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <param name="project">Unique key associated with the Sonar project</param>
+        /// <param name="path">Local hint path associated with the spcified key, if the latter is found</param>
+        void TryGetvalue(string project, out string path);
+    }
+}
